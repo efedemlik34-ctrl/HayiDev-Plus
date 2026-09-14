0ad29a1442002ec4d8ab0c29794706f6f01682f5
unknown <Administrator@244412249-190921897-390720005.244412249-190921897-390720005>
2026-09-14 20:13:55 +0300

    Fix settings.gradle.kts: dependencyResolution -> dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,8 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolution {
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     @Suppress("UnstableApiUsage")
     repositories {
         google()
